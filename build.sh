1985f2b75d2bc7e603eb79d52d368b9b31465948
V. Shcherbakov <vitya.shcherbakov@gmail.com>
2022-06-15 22:19:46 +0400

    minor script fixes

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-/usr/bin/time -f "\nTotal compilation time: %e sec" /bin/sh -c 'CC=gcc CXX=/usr/bin/g++ cmake . && cmake --build . -- -j $(nproc) && ctest'
+/usr/bin/time -f "\nTotal compilation time: %e sec" /bin/sh -c 'CC=gcc CXX=/usr/bin/g++ cmake . && cmake --build . -- -j $(nproc) && ctest' &&
+mv qkd/app $(pwd)
